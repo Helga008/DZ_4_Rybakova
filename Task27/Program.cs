4c56faa4ead975e1ffb25de2d8c0364241525d91
Olga <olgarybakova@bk.ru>
2023-01-15 20:21:00 +0300

    Задача 27

---
+++ Task27/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число и выдает сумму цифр в этом числе
+
+int User(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+int SumDigitInNumb(int number)
+{ 
+    int sum = 0;
+    while (number > 0)
+    {     
+        int num = number % 10;
+        sum = sum + num;
+        number = number / 10;
+    }
+    return sum;
+}
+
+int number1 = User("Введите число: ");
+int Sum = SumDigitInNumb(number1); 
+Console.WriteLine(Sum);
